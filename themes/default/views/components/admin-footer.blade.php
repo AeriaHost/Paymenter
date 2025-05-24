bd9c750cfb176ef271eb832665b62b660c71571a
Khisab Kurniawan <khisabkurniawan@outlook.com>
2025-05-24 11:59:10 +0700

    adjust footer

--- themes/default/views/components/admin-footer.blade.php
+++ themes/default/views/components/admin-footer.blade.php
@@ -2,8 +2,10 @@
 </div>
 
 <span class="text-xs text-gray-400 mt-auto">
-    Powered by <a href="https://paymenter.org" target="_blank" class="text-gray-400">Paymenter &copy;
-        {{ date('Y') }}</a>
+    Powered by 
+    <a href="https://paymenter.org" target="_blank" class="text-gray-400 hover:underline">Paymenter &copy; {{ date('Y') }}</a> 
+    | Modified by 
+    <a href="https://billing.aeria.host" target="_blank" class="text-gray-400 hover:underline">Aeromenter</a>
 </span>
 
 <ul class="fi-sidebar-nav-groups -mx-2 flex flex-col gap-y-7">
